75d89491fe863d85085429cea10b3d527e9dcb19
David Araújo <david2araujo5@gmail.com>
2023-07-27 20:25:24 +0100

    Container upgraded

--- start_switch.sh
+++ start_switch.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env sh
 
-PORT_NUM=$1     # Number of ports of the switch
-GRPC_PORT=$2    # gRPC port number
+PORT_NUM=$(expr $1 + 1) # Number of ports of the switch
+GRPC_PORT=$2            # gRPC port number
 
 INTFS_ARG=""
-for port in $(seq 0 $PORT_NUM); do
-    intf="macvlan$port"
+for port in $(seq 1 $PORT_NUM); do
+    intf="port$port"
     if ! ip link show $intf &> /dev/null; then
         ip link add $intf link eth0 type macvlan mode vepa  
         ip link set dev $intf up
+        ip addr add 192.168.$port.3/24 dev $intf
+        ip route add default via 192.168.$port.1 dev $intf
         INTFS_ARG="$INTFS_ARG-i $port@$intf "
     fi
 done
